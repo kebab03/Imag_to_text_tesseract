4c8a6366f630c285bde3476e180d79118c7ea31f
kebab03 <68882205+kebab03@users.noreply.github.com>
2024-11-19 07:41:19 +0100

    ita

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
 import pytesseract
 from PIL import Image
-import os
 
-# Set the TESSDATA_PREFIX environment variable
-os.environ['TESSDATA_PREFIX'] = r"C:\Tesseract-OCR\tessdata"
+def extract_text(img_path):
+    try:
+        img = Image.open(img_path)
+        custom_config = r'--oem 3 --psm 6'
+        text = pytesseract.image_to_string(img, lang='ita', config=custom_config)
+        return text
+    except Exception as e:
+        print(f"Errore durante l'estrazione del testo: {e}")
+        return None
 
-# Define the path to the Tesseract executable
-pytesseract.pytesseract.tesseract_cmd = r"C:\Tesseract-OCR\tesseract.exe"
-string = r"C:\Users\user\Pictures\Camera Roll"
-# replace all instances of 'r' (old) with 'e' (new)
-new_string = string.replace("\\", "/")
-print("new_string:::-",new_string)
-fil="2023-07-01 04_05_29-How to use ScreenManager and Screens with Kivy and KivyMD _ Python _ Kivy _ Kivy.png"
-prin=new_string+"/"+fil
-print("prin:-",prin)
-# Open the image and store it in an image object
-img = Image.open(prin)
+img_path = 'C:\\Users\\Sharif\\Pictures\\tesseract\\myp.png'
+extracted_text = extract_text(img_path)
 
-# Extract text from the image
-text = pytesseract.image_to_string(img)
-
-# Display the extracted text
-print(text)
+if extracted_text:
+    print(extracted_text)
+else:
+    print("Non è stato possibile estrarre il testo.")
